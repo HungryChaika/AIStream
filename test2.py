fde02f097771a40a9a3c8eea2cf3c77de95ded61
unknown <vologzhanin_00@mail.ru>
2025-03-20 17:09:37 +0400

    Add GPU support

--- test2.py
+++ test2.py
@@ -1,5 +1,11 @@
 import cv2
+import torch
 from ultralytics import YOLO
+
+print(torch.cuda.is_available())
+device = "0" if torch.cuda.is_available() else "cpu"
+if device == "0":
+    torch.cuda.set_device(0)
 
 model = YOLO("yolo11x.pt")
 results = model.track(source='rtsp://admin:Qwerty123@192.168.1.33:554/cam/realmonitor?channel=1&subtype=1', show=True)
@@ -7,7 +13,7 @@ video_cap = cv2.VideoCapture(results)
 
 while True:
     ret, frame = video_cap.read()
-    model.cpu()
+    model.cuda()
 
     if not ret:
         break
