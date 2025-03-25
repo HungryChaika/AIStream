a3633994282e73d3121c98081b78ea79d6fc2eb9
unknown <vologzhanin_00@mail.ru>
2025-03-25 13:49:39 +0400

    track with out frame

--- test2.py
+++ test2.py
@@ -8,20 +8,8 @@ if device == "0":
     torch.cuda.set_device(0)
 
 model = YOLO("yolo11x.pt")
-results = model.track(source='rtsp://admin:Qwerty123@192.168.1.33:554/cam/realmonitor?channel=1&subtype=1', show=True)
+results = model.track(source="rtsp://admin:Qwerty123@192.168.1.72:554/cam/realmonitor?channel=9&subtype=1", show=True)
 video_cap = cv2.VideoCapture(results)
-
-while True:
-    ret, frame = video_cap.read()
-    model.cuda()
-
-    if not ret:
-        break
-
-    cv2.imshow("Frame", frame)
-
-    if cv2.waitKey(1) == ord("q"):
-        break
 
 video_cap.release()
 cv2.destroyAllWindows()
