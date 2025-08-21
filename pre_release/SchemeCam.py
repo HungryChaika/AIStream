9a509a9ef5b514c88d425c4e419df9f47656ad88
unknown <vologzhanin_00@mail.ru>
2025-08-21 18:21:55 +0400

    Save cam and view cams + cams json

--- pre_release/SchemeCam.py
+++ pre_release/SchemeCam.py
@@ -1,10 +1,10 @@
 import tkinter as tk
 from tkinter import filedialog
 import json
+import uuid
 from pprint import pformat
 from os import path
 from math import atan2, degrees
-# import math
 
 def points(el):
     if "points" in el["XY"][0]:
@@ -14,17 +14,19 @@ def points(el):
 
 class SchemeCam():
     def __init__(self):
-        self.j = self.__open_json()
+        self.j = self.__open_json("6k3f")
+        self.j_cam = self.__open_json("cams_position")
         self.scale = 18
         self.cameras = {}
         self.canvas_floor = []  # пары этаж-canvas
+        self.canvas_cams = []   # пары canvas-камеры
         self.min_x, self.min_y, self.max_x, self.max_y = self.__calc_max_min()
         self.offset_x, self.offset_y = -self.min_x, -self.min_y
         self.__process()
 
-    def __open_json(self):
+    def __open_json(self, name):
         ''' Открытие json, имеющиего информацию наблюдаемой территории '''
-        filename = rf"{path.dirname(__file__)}/res/6k3f.json"
+        filename = rf"{path.dirname(__file__)}/res/{name}.json"
         filename.replace("\\", "/")
         try:
             with open(filename) as f:
@@ -57,19 +59,20 @@ class SchemeCam():
         ''' Перевод из координат canvas в координаты здания '''
         return (x / self.scale - self.offset_x), (-y / self.scale + self.offset_y)
     
-    def __create_camera_user(self, canvas, floor_name, floor_level):
+    def __create_camera(self, canvas, name_level, floor_level):
         camera = CameraCanvas(canvas.create_oval(0, 0, 0, 0, fill = "#FF0000", outline = "#000000"), 
-                              camera_radius = min(self.max_x * 0.05 * self.scale / 2, self.max_y * 0.05 * self.scale / 2))
+                              camera_radius = min(self.max_x * 0.05 * self.scale / 2, self.max_y * 0.05 * self.scale / 2),
+                              floor_level=floor_level, name_level=name_level)
         canvas.bind("<Motion>", lambda event: self.__event_move_cam(event, camera, canvas))
-        canvas.bind("<Button-1>", lambda event: self.__event_place_cam(event, camera, canvas, floor_name, floor_level))
-        canvas.bind("<Tab>", lambda event: self.__event_resetinging_cam_placement(event, camera, canvas, floor_name, floor_level))
+        canvas.bind("<Button-1>", lambda event: self.__event_place_cam(event, camera, canvas))
+        canvas.bind("<Tab>", lambda event: self.__event_resetinging_cam_placement(event, camera, canvas, name_level, floor_level))
 
     def __event_move_cam(self, event, camera, canvas):
         ''' Привязка камеры к курсору мыши '''
         x, y = canvas.canvasx(event.x), canvas.canvasy(event.y)
         canvas.coords(camera.camera, x - camera.camera_radius, y - camera.camera_radius, x + camera.camera_radius, y + camera.camera_radius)
     
-    def __event_place_cam(self, event, camera, canvas, floor_name, floor_level):
+    def __event_place_cam(self, event, camera, canvas):
         canvas.unbind("<Motion>")
         canvas.unbind("<Button-1>")
         x, y = canvas.canvasx(event.x), canvas.canvasy(event.y)
@@ -80,43 +83,90 @@ class SchemeCam():
                                               (x + camera.camera_radius * 2, y + camera.camera_radius * 2), 
                                               fill="#FF0000", outline="#000000"))
         canvas.bind("<Motion>", lambda event: self.__event_rotation_cams_field_view(event, camera, canvas))
-        canvas.bind("<Button-1>", lambda event: self.__event_place_field_view(event, camera, canvas, floor_name, floor_level))
+        canvas.bind("<Button-1>", lambda event: self.__event_place_field_view(event, camera, canvas))
 
     def __event_rotation_cams_field_view(self, event, camera, canvas):
         canvas.itemconfig(camera.field_of_view, start = degrees(
             atan2(canvas.canvasx(event.x) - camera.center_coord["x"], canvas.canvasy(event.y) - camera.center_coord["y"])) + 225)
 
-    def __event_place_field_view(self, event, camera, canvas, floor_name, floor_level):
+    def __event_place_field_view(self, event, camera, canvas):
         canvas.unbind("<Motion>")
         canvas.unbind("<Button-1>")
         canvas.tag_raise(camera.camera)
         canvas.itemconfig(camera.field_of_view, fill="#00FF00")
-        print(floor_name, floor_level)
+        data = {
+            "FloorLevel": camera.floor_level,
+            "NameLevel": camera.name_level,
+            "CenterX": camera.build_center_ccord["x"],
+            "CenterY": camera.build_center_ccord["y"],
+            "AngleOfRotation": canvas.itemcget(camera.field_of_view, "start")
+        }
+        self.__data_filling_window(data)
 
+    def __data_filling_window(self, data):
+        top = tk.Toplevel()
+        top.geometry("300x300")
+        frame = tk.Frame(top)
+        frame.pack(expand=True, fill="both", anchor="center")
+        label_name = tk.Label(frame, text="Введите название:")
+        entry_name = tk.Entry(frame)
+        label_address = tk.Label(frame, text="Введите место установки:")
+        entry_location = tk.Entry(frame)
+        label_name.pack()
+        entry_name.pack()
+        label_address.pack()
+        entry_location.pack()
+        tk.Button(frame, text="Ввод", command=lambda entry_name = entry_name, entry_location = entry_location, 
+                  data = data, top = top : self.__event_data_filling(entry_name, entry_location, data, top)).pack()
 
+    def __event_data_filling(self, entry_name, entry_location, data, top):
+        if(entry_name.get()):
+            data["Name"] = entry_name.get()
+            data["Location"] = entry_location.get()
+            top.destroy()
+            name_f = "cams_position"
+            for lvl in self.j_cam["Level"]:
+                if lvl["ZLevel"] == data["FloorLevel"] and lvl["NameLevel"] == data["NameLevel"]:
+                    lvl["BuildElement"].append(
+                        { "Name": data["Name"], "Id": f"{uuid.uuid1()}", "Sign": "Camera", "AngleOfRotation": data["AngleOfRotation"], "Location": data["Location"], "SizeZ": 0.0, "XY": [{ "points": [{ "x": data["CenterX"], "y": data["CenterY"] }] }] }
+                    )
+                break
+            with open(path.join(rf"{path.dirname(__file__)}/res/", f"{name_f}.json"), "w") as json_file:
+                json.dump(self.j_cam, json_file, ensure_ascii=False, indent=4)
 
-        self.test(camera, canvas)
-
-
-
-    def test(self, camera, canvas):
-        canvas.tag_bind(camera.camera, "<Enter>", lambda event: self.aaaa(event, camera, canvas, fill="#0000FF"))
-        canvas.tag_bind(camera.camera, "<Leave>", lambda event: self.aaaa(event, camera, canvas, fill="#00FF00"))
-        canvas.tag_bind(camera.field_of_view, "<Enter>", lambda event: self.aaaa(event, camera, canvas, fill="#0000FF"))
-        canvas.tag_bind(camera.field_of_view, "<Leave>", lambda event: self.aaaa(event, camera, canvas, fill="#00FF00"))
-
-    def aaaa(self, event, camera, canvas, fill):
-        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-        canvas.itemconfig(camera.camera, fill=fill)
-        canvas.itemconfig(camera.field_of_view, fill=fill)
-
-
-
-
-
-    def __event_resetinging_cam_placement(self, event, camera, canvas, floor_name, floor_level):
+    def __event_resetinging_cam_placement(self, event, camera, canvas, name_level, floor_level):
         camera.destroy(canvas)
-        self.__create_camera_user(canvas, floor_name, floor_level)
+        self.__create_camera(canvas, name_level, floor_level)
+
+    def __place_all_cameras(self, canvas, name_level, floor_level):
+        name_f = "cams_position"
+        camera_radius = min(self.max_x * 0.05 * self.scale / 2, self.max_y * 0.05 * self.scale / 2)
+        for lvl in self.j_cam["Level"]:
+            if lvl["ZLevel"] == floor_level and lvl["NameLevel"] == name_level:
+                camera_center = {}
+                for el in lvl["BuildElement"]:
+                    for x, y in points(el):
+                        camera_center["x"], camera_center["y"] = self.__crd(x, y)
+                    camera = CameraCanvas(
+                            camera = canvas.create_oval( 
+                                    camera_center["x"] - camera_radius, camera_center["y"] - camera_radius, 
+                                    camera_center["x"] + camera_radius, camera_center["y"] + camera_radius, 
+                                    fill = "#00FF00", outline = "#000000"), 
+                            camera_radius = camera_radius,
+                            field_of_view = canvas.create_arc( 
+                                    (camera_center["x"] - camera_radius * 2, camera_center["y"] - camera_radius * 2), 
+                                    (camera_center["x"] + camera_radius * 2, camera_center["y"] + camera_radius * 2), 
+                                    fill = "#00FF00", outline = "#000000", start = el["AngleOfRotation"]), 
+                            floor_level = floor_level, name_level = name_level)
+                    canvas.tag_bind(camera.camera, "<Enter>", lambda event, camera=camera: change_color(event, camera, canvas, fill="#0000FF"))
+                    canvas.tag_bind(camera.camera, "<Leave>", lambda event, camera=camera: change_color(event, camera, canvas, fill="#00FF00"))
+                    canvas.tag_bind(camera.field_of_view, "<Enter>", lambda event, camera=camera: change_color(event, camera, canvas, fill="#0000FF"))
+                    canvas.tag_bind(camera.field_of_view, "<Leave>", lambda event, camera=camera: change_color(event, camera, canvas, fill="#00FF00"))
+                    self.canvas_cams.append((canvas, camera))
+
+        def change_color(event, camera, canvas, fill):
+            canvas.itemconfig(camera.camera, fill=fill)
+            canvas.itemconfig(camera.field_of_view, fill=fill)  
 
     def __process(self):
         ''' Отрисовка схемы территории '''
@@ -141,14 +191,14 @@ class SchemeCam():
             except tk.TclError:
                 top.wm_state("zoomed")
             self.canvas_floor.append((lvl, canvas))
-            
         # Рисуем фон
         colors = {"Room": "", "DoorWayInt": "yellow", "DoorWayOut": "brown", "DoorWay": "", "Staircase": "green"}
         for lvl, canvas in self.canvas_floor:
             for el in lvl['BuildElement']:
                 polygon = canvas.create_polygon([self.__crd(x, y) for x, y in points(el)], fill=colors[el['Sign']], outline='black')
-                # canvas.tag_bind(polygon, "<Button-1>", lambda e, el=el: tk.messagebox.showinfo("Инфо об объекте", pformat(el, compact=True, depth=2)))
-            self.__create_camera_user(canvas, lvl["NameLevel"], lvl["ZLevel"])
+                # canvas.tag_bind(polygon, "<Button-1>", lambda e, el=el: tk.messagebox.showinfo("Инфо об объекте", pformat(el, compact=True, depth=5)))
+            # self.__create_camera(canvas, lvl["NameLevel"], lvl["ZLevel"])
+            self.__place_all_cameras(canvas, lvl["NameLevel"], lvl["ZLevel"])             
 
 class App(tk.Tk):
     def __init__(self):
@@ -159,7 +209,7 @@ class App(tk.Tk):
         self.mainloop()
 
 class CameraCanvas():
-    def __init__(self, camera, field_of_view = None, camera_radius = None, field_of_view_radius = None):
+    def __init__(self, camera, field_of_view = None, camera_radius = None, field_of_view_radius = None, name_level = None, floor_level = None):
         self.camera = camera
         self.field_of_view = field_of_view
         self.center_coord = {
@@ -172,14 +222,15 @@ class CameraCanvas():
         }
         self.camera_radius = camera_radius
         self.field_of_view_radius = field_of_view_radius
+        self.name_level = name_level
+        self.floor_level = floor_level
 
     def set_field_of_view(self, field_of_view):
         self.field_of_view = field_of_view
 
-    def set_camera_radius(self, x, y):
-        self.camera_radius["x"] = x
-        self.camera_radius["y"] = y
-
+    def set_camera_radius(self, r):
+        self.camera_radius = r
+        
     def set_camera_center_coord(self, x, y):
         self.center_coord["x"] = x
         self.center_coord["y"] = y
